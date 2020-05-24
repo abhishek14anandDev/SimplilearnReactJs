199536f63ede9b55a69b5a718a812457609ae930
Abhishek Anand <abhishek14anand@live.com>
2020-05-24 09:02:40 +0530

    sbdfgsd

--- Array.js
+++ Array.js
@@ -5,3 +5,16 @@ console.log(typeof items);
 var items1 = new Array()
 
 console.log(typeof items1);
+
+var items = ['pen', 'pencil','eraser'];
+
+console.log(items[0]);
+
+for(var i =0; i<items.length; i++)
+    console.log(items[i]);
+
+for(let item of items)
+    console.log(item);
+
+for(var i in items)
+    console.log(items[i]);
---
+++ jsonFormat.js
@@ -0,0 +1,10 @@
+var o = {"col1": "Abhishek", "col2":"Anand"}
+console.log(o);
+console.log(`${o.col1} ${o.col2}`);
+
+for(let i in o)
+{
+    console.log(i);
+    console.log(o[i]);
+}
+    
